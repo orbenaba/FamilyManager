aa5e27e363ada629a2e61c040996cd73d8ec3a13
orbenaba <orbenabba@gmail.com>
2020-04-29 02:00:59 +0300

    Add a transition between "create family" and "create user in family". COMPILED-YES RUNNING-YES

---
+++ src/Views/RegisterHumanView.java
@@ -0,0 +1,21 @@
+package Views;
+
+import Models.Address;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.Calendar;
+
+public class RegisterHumanView extends JFrame {
+    public static void displayHumanForm(JFrame frame)
+    {
+        JLabel firstnameLabel,addressLabel,genderIdLabel,birthday,image,bioFileName;
+        JTextField firstname;
+
+
+
+
+
+
+    }
+}
