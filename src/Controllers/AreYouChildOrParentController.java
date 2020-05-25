e4f44b49107aae314b72e49c634b3e08ae3f158e
orbenaba <orbenabba@gmail.com>
2020-05-25 20:54:48 +0300

    Design AreYouChildOrParentView view perfectly.
    Signed-off-by: orbenaba <orbenabba@gmail.com>

--- src/Controllers/AreYouChildOrParentController.java
+++ src/Controllers/AreYouChildOrParentController.java
@@ -3,18 +3,21 @@ package Controllers;
 import Views.AreYouChildOrParentView;
 import Views.RegisterChildView;
 import Views.RegisterParentView;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import static Views.RegisterView.*;
 
 public class AreYouChildOrParentController extends JframeController{
     private AreYouChildOrParentView cpview;
     public AreYouChildOrParentController(AreYouChildOrParentView cpview){
         super(cpview);
         this.cpview=cpview;
-        changeColor(new RegisterController.changeButtonColor(cpview.child),cpview.child);//change the colors of both buttons when hovering above em
-        changeColor(new RegisterController.changeButtonColor(cpview.parent),cpview.parent);
+        cpview.addChildListener(new ChildListener());
+        cpview.addParentListener(new ParentListener());
         cpview.addActionChild(new ChildAction());
         cpview.addActionParent(new ParentAction());
     }
@@ -31,6 +34,30 @@ public class AreYouChildOrParentController extends JframeController{
         public void actionPerformed(ActionEvent e) {
             new RegisterParentController(new RegisterParentView(cpview.familyUsername));
             cpview.dispose();
+        }
+    }
+
+    class ChildListener extends MouseAdapter{
+        @Override
+        public void mouseExited(MouseEvent e) {
+            cpview.child.setBackground(new Color(0,7,204));
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            cpview.child.setBackground(new Color(0,101,183));
+        }
+    }
+
+    class ParentListener extends MouseAdapter{
+        @Override
+        public void mouseExited(MouseEvent e) {
+            cpview.parent.setBackground(new Color(0,7,204));
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            cpview.parent.setBackground(new Color(0,101,183));
         }
     }
 
--- src/Views/AreYouChildOrParentView.java
+++ src/Views/AreYouChildOrParentView.java
@@ -4,29 +4,47 @@ package Views;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-
-import static Views.RegisterView.addTitle;
+import java.awt.event.MouseAdapter;
 
 public class AreYouChildOrParentView extends Jframe {
     public JLabel title,title2;
     public JButton child,parent;
     public String familyUsername;
-
-
-
+    int width=700,height=600;
     public AreYouChildOrParentView(String familyUsername){
-        super(500);
+        super(700);
         this.familyUsername=familyUsername;
-        setSize(500,500);
+        setSize(700,600);
         setLocationRelativeTo(null);
-        getContentPane().setBackground(new Color(85, 246, 44));
+        getContentPane().setBackground(Color.orange);
         title=new JLabel("Are you considered as");
-        addTitle(title,10,15,400,80);
+
+        //Label for "Register" title
+        title.setFont(new Font("David",Font.ITALIC,40));
+        title.setForeground(Color.white);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setVerticalAlignment(SwingConstants.CENTER);
+        title.setBackground(new Color(0,7,204));
+        title.setOpaque(true);//The default is the background is transparent
+        title.setBounds(width/2-275,15,500,80);
         title2=new JLabel("a child or as a parent?");
-        addTitle(title2,10,95,400,80);
+        //Label for "Register" title
+        title2.setFont(new Font("David",Font.ITALIC,40));
+        title2.setForeground(Color.white);
+        title2.setHorizontalAlignment(SwingConstants.CENTER);
+        title2.setVerticalAlignment(SwingConstants.CENTER);
+        title2.setBackground(new Color(0,7,204));
+        title2.setOpaque(true);//The default is the background is transparent
+        title2.setBounds(width/2-275,95,500,80);
+        title.setBorder(BorderFactory.createMatteBorder(8,8,0,8,new Color(153, 0, 92)));
+        title2.setBorder(BorderFactory.createMatteBorder(0,8,8,8,new Color(153, 0, 92)));
+
+
+
+
         child=new JButton("Child");
         parent=new JButton("Parent");
-        addButtons(15,200,400,142);
+        addButtons(width/2-275,230,550,150);
         add(child);
         add(parent);
         add(title2);
@@ -34,16 +52,21 @@ public class AreYouChildOrParentView extends Jframe {
         setVisible(true);
     }
     public void addButtons(int x,int y,int width,int height){
-        Font f=new Font("Arial",Font.BOLD,30);
+        Font f=new Font("David",Font.ITALIC,40);
         Cursor c=new Cursor(Cursor.HAND_CURSOR);
         child.setBounds(x,y,width,height);
-        parent.setBounds(x,y+height+10,width,height);
+        parent.setBounds(x,y+height+30,width,height);
         child.setFont(f);
         parent.setFont(f);
         child.setCursor(c);
         parent.setCursor(c);
-        child.setBackground(new Color(0,84,104));
-        parent.setBackground(new Color(0,84,104));
+        parent.setFocusPainted(false);
+        child.setFocusPainted(false);
+        child.setBorder(BorderFactory.createMatteBorder(6,6,6,6,new Color(153, 0, 92)));
+        parent.setBorder(BorderFactory.createMatteBorder(6,6,6,6,new Color(153, 0, 92)));
+
+        child.setBackground(new Color(0,7,204));
+        parent.setBackground(new Color(0,7,204));
         child.setForeground(Color.white);
         parent.setForeground(Color.white);
     }
@@ -52,5 +75,13 @@ public class AreYouChildOrParentView extends Jframe {
     }
     public void addActionParent(ActionListener mal){
         parent.addActionListener(mal);
+    }
+
+    public void addChildListener(MouseAdapter mal){
+        child.addMouseListener(mal);
+    }
+
+    public void addParentListener(MouseAdapter mal){
+        parent.addMouseListener(mal);
     }
 }
